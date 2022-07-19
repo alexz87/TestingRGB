30c5ce263757e5cb15faf6da543144229822cdd7
Oleksandr <zadorozhniy3@icloud.com>
2022-07-20 01:34:29 +0300

    mod main

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
       colors: {
         leftBox: "#F8F8F8",
         activeColor: "#4D49B4",
-        header: "#F3F5F6"
+        header: "#F3F5F6",
+        notification: "#EF8566",
+        sphere: "#8F92A5",
+        firstChild: "#16113D",
+        lastChild: "#8F92A5",
+        main: "#F5F5F5"
       }
     },
   },
