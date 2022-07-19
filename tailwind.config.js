c4929864b5f9109681792467318b1776d07c859f
Oleksandr <zadorozhniy3@icloud.com>
2022-07-20 00:18:33 +0300

    add tailwind & mod style

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        leftBox: "#F8F8F8",
+        activeColor: "#4D49B4"
+      }
+    },
+  },
+  plugins: [],
+}
